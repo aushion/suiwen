beec4b358d49c3df0634d42d4671300ff2f2e7fa
aushion <783435110@qq.com>
2020-05-21 13:57:16 +0800

    解决机构类型账户登录无限刷新的问题

--- src/global.js
+++ src/global.js
@@ -1,6 +1,6 @@
 import RestTools from './utils/RestTools';
 import Cookies from 'js-cookie';
-const domain = window.document.domain;
+
 
 if (!Cookies.get('cnki_qa_uuid')) {
   Cookies.set('cnki_qa_uuid', RestTools.createUid(), {
@@ -8,13 +8,13 @@ if (!Cookies.get('cnki_qa_uuid')) {
   });
 }
 
-if (Cookies.get('Ecp_LoginStuts')) {
-  window.localStorage.setItem('userInfo', Cookies.get('Ecp_LoginStuts'));
-} else {
-  if (domain.endsWith('cnki.net')) {
-    window.localStorage.setItem('userInfo', null);
-  }
-}
+// if (Cookies.get('Ecp_LoginStuts')) {
+//   window.localStorage.setItem('userInfo', Cookies.get('Ecp_LoginStuts'));
+// } else {
+//   if (domain.endsWith('cnki.net')) {
+//     window.localStorage.setItem('userInfo', null);
+//   }
+// }
 
 if (/Android|Windows Phone|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
   window.location.href = 'http://qa.cnki.net/sw.mobile';
