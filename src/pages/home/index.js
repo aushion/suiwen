49f106f238fac64a40a8799027580afd51d97157
chenaosheng <chenaosheng@gmail.com>
2020-11-18 18:06:07 +0800

    合并法律专题到专题详情页

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -168,7 +168,7 @@ function Home(props) {
                 return (
                   <Link
                     className={homeStyles.questions_item}
-                    to={`/query`+(item.name === '法律' ? `/law`:``)+`?topic=${item.info.topic}&topicName=${encodeURIComponent(
+                    to={`/query?topic=${item.info.topic}&topicName=${encodeURIComponent(
                       item.name
                     )}&q=${encodeURIComponent(child.question)}`}
                     key={child.qId}
