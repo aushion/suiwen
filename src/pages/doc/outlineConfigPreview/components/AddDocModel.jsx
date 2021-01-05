70ba8e74601096ee8b96fa2bb683f3bee3b82554
songjiang <SONGJIANG>
2021-01-05 14:10:12 +0800

    为新增文档添加文档标题内容校验

--- src/pages/doc/outlineConfigPreview/components/AddDocModel.jsx
+++ src/pages/doc/outlineConfigPreview/components/AddDocModel.jsx
@@ -1,43 +1,49 @@
 import React, { useState } from 'react';
-import { Input, Modal, Form, Select, Radio, Button } from 'antd';
+import { Input, Modal, Form, Select, Radio, Button,message } from 'antd';
 
 const AddDocModel = Form.create({
   mapPropsToFields(props) {
     if (props.data !== undefined) {
-      return {
-      };
+      return {};
     }
-  },
-})(props => {
-
-
-  const { modalVisible, form, docTemplateOptions, defaultDocumentTemplate, docClassifyOptions, newType } = props;
+  }
+})((props) => {
+  const {
+    modalVisible,
+    form,
+    docTemplateOptions,
+    defaultDocumentTemplate,
+    docClassifyOptions,
+    newType
+  } = props;
   //文档模版选择
-  const [selectedDocTemplate, setSeletedDocTemplate] = useState(defaultDocumentTemplate ? defaultDocumentTemplate : '');
+  const [selectedDocTemplate, setSeletedDocTemplate] = useState(
+    defaultDocumentTemplate ? defaultDocumentTemplate : ''
+  );
   //文档标题
   const [docTitle, setDocTitle] = useState('');
 
   const formItemLayout = {
     labelCol: {
       xs: { span: 24 },
-      sm: { span: 4 },
+      sm: { span: 4 }
     },
     wrapperCol: {
       xs: { span: 24 },
-      sm: { span: 20 },
-    },
+      sm: { span: 20 }
+    }
   };
 
   const onHandleCancel = () => {
     props.onHandleCancel();
-  }
+  };
   //文档模板选择改变触发函数
   const onDocTemplateSelectChange = (v) => {
     setSeletedDocTemplate(v);
     if (v === '') {
       return;
     }
-  }
+  };
   //主题词输入内容改变触发函数
   const onKeyWordChange = (e) => {
     //将文档标题随着主题词的改变而改变。规则分为两种：
@@ -47,29 +53,34 @@ const AddDocModel = Form.create({
     if (newType === 1) {
       setDocTitle(keyWordTemp);
     } else if (newType === 2) {
-      var currentDate = new Date().Format("yyyyMMddHHmmss");
+      var currentDate = new Date().Format('yyyyMMddHHmmss');
       let newDocTitleTemp = keyWordTemp + 'temp_' + currentDate;
       setDocTitle(newDocTitleTemp);
     } else {
       return;
     }
-
-  }
+  };
 
   //文档标题输入内容改变触发函数
   const onDocTitleChange = (e) => {
     setDocTitle(e.target.value);
-  }
+  };
 
   //新建文档，提交按钮事件
   const onHandleOk = () => {
+    //校验文档标题
+    if (docTitle.trim() === '') {
+      message.warn('文档标题不可为空！');
+      return;
+    }
+
     form.validateFields((err, fieldsValue) => {
       if (err) return;
       const values = {
         ...fieldsValue,
-        'docTemplateId': selectedDocTemplate,
-        'label': docTitle,
-      }
+        docTemplateId: selectedDocTemplate,
+        label: docTitle
+      };
       props.onHandleOk(values);
     });
   };
@@ -86,11 +97,9 @@ const AddDocModel = Form.create({
       width={600}
       onOk={onHandleOk}
       onCancel={() => onHandleCancel()}
-
     >
-      <Form {...formItemLayout} >
-
-        <Form.Item label="文档模版" >
+      <Form {...formItemLayout}>
+        <Form.Item label="文档模版">
           <Select
             style={{ width: 400 }}
             value={selectedDocTemplate}
@@ -102,15 +111,24 @@ const AddDocModel = Form.create({
         </Form.Item>
         <Form.Item label="主题词" hidden={selectedDocTemplate === '' ? true : false}>
           {form.getFieldDecorator('keyWord', {
-            rules: [{ pattern: /^(\S[\S\s]{0,19})$/, required: selectedDocTemplate === '' ? false : true, message: '开头不可为空字符,且不可超过20个字符' }],
-          })(<Input placeholder='文档模板不为空时，主题词必填' style={{ width: 400 }} maxLength={30} onChange={(e) => onKeyWordChange(e)} />)}
+            rules: [
+              {
+                pattern: /^(\S[\S\s]{0,19})$/,
+                required: selectedDocTemplate === '' ? false : true,
+                message: '开头不可为空字符,且不可超过20个字符'
+              }
+            ]
+          })(
+            <Input
+              placeholder="文档模板不为空时，主题词必填"
+              style={{ width: 400 }}
+              maxLength={30}
+              onChange={(e) => onKeyWordChange(e)}
+            />
+          )}
         </Form.Item>
-        <Form.Item label="文档标题">
-          {/* {form.getFieldDecorator('label', {
-            rules: [{ pattern: /^(.{1,30})$/, required: true, message: '文档标题不可超过30位字符!' }],
-          })(<Input placeholder='建议中文、数字与下划线"_" ' style={{ width: 400 }} maxLength={30} />)} */}
+        <Form.Item label="文档标题" required={true}>
           <Input
-            required={true}
             value={docTitle}
             placeholder='建议中文、数字与下划线"_" '
             style={{ width: 400 }}
@@ -119,10 +137,9 @@ const AddDocModel = Form.create({
           />
         </Form.Item>
         <Form.Item label="文档标签">
-          {form.getFieldDecorator('tag', {
-          })(
+          {form.getFieldDecorator('tag', {})(
             <Select
-              placeholder='点击此处可为文档选取多个标签'
+              placeholder="点击此处可为文档选取多个标签"
               mode="multiple"
               style={{ width: 400 }}
             >
@@ -140,19 +157,15 @@ const AddDocModel = Form.create({
               <Radio value="0">公开</Radio>
               <Radio value="1">私有</Radio>
             </Radio.Group>
-
           )}
           <Button
-            style={{ border: "0px", marginLeft: "230px" }}
-            size={"small"}
-            icon={"info-circle"}
-            title={"选择公开，表示文档可以发布，使他人可见；\r\n选择私有，代表文档只自己可见。"}
-            onClick={() => {
-            }}
-          >
-          </Button>
+            style={{ border: '0px', marginLeft: '230px' }}
+            size={'small'}
+            icon={'info-circle'}
+            title={'选择公开，表示文档可以发布，使他人可见；\r\n选择私有，代表文档只自己可见。'}
+            onClick={() => {}}
+          ></Button>
         </Form.Item>
-
       </Form>
     </Modal>
   );
